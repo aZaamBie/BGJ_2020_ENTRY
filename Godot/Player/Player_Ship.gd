0ec3afae51201b6720168186600e7cfdf37406f2
aZaamBie <azaamholdman@Gmail.com>
2020-02-23 07:54:21 +0200

    Black Holes, improved Camera
    I've now added Black Holes. Currently very small, but I will increase size later.
    Also fixed the camera to follow the player

--- Godot/Player/Player_Ship.gd
+++ Godot/Player/Player_Ship.gd
@@ -1,28 +1,23 @@
 extends KinematicBody2D
 
 # variables
-#export var Speed = 0
-#export var MaxSpeed = 0
-#var direction = Vector2()
-#
-#func _process(delta):
-#	look_at(get_global_mouse_position()) # looks at mouse
-#
-#	# keyboard movement
-#	if Input.is_action_pressed("UP"):
-#		direction += Vector2(0,-1)
-#	if Input.is_action_pressed("DOWN"):
-#		direction += Vector2(0,1)
-#	if Input.is_action_pressed("LEFT"):
-#		direction += Vector2(-1,0)
-#	if Input.is_action_pressed("RIGHT"):
-#		direction += Vector2(1,0)
-#	move_and_slide(direction * Speed)
-	
-	# Speed boost
+export var Speed = 3
+export var MaxSpeed = 0
+var direction = Vector2()
 
+func _process(delta):
+	look_at(get_global_mouse_position()) # looks at mouse
 
-
+	# keyboard movement
+	if Input.is_action_pressed("UP"):
+		direction += Vector2(0,-1)
+	if Input.is_action_pressed("DOWN"):
+		direction += Vector2(0,1)
+	if Input.is_action_pressed("LEFT"):
+		direction += Vector2(-1,0)
+	if Input.is_action_pressed("RIGHT"):
+		direction += Vector2(1,0)
+	move_and_slide(direction * Speed)
 
 #export (int) var speed = 200
 #
