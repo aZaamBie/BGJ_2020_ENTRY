ff8843d64edc2443b5ccedfd4998507d2075228b
aZaamBie <azaamholdman@Gmail.com>
2020-02-23 07:08:36 +0200

    Infinite background added
    I've added an infinite space background and improved the space ship controls

--- Godot/Player/Player_Ship.gd
+++ Godot/Player/Player_Ship.gd
@@ -1,27 +1,42 @@
 extends KinematicBody2D
 
 # variables
-export var Speed = 0
-export var MaxSpeed = 0
-var direction = Vector2()
-
-func _ready():
-	pass
-
-func _process(delta):
-	look_at(get_global_mouse_position()) # looks at mouse
+#export var Speed = 0
+#export var MaxSpeed = 0
+#var direction = Vector2()
+#
+#func _process(delta):
+#	look_at(get_global_mouse_position()) # looks at mouse
+#
+#	# keyboard movement
+#	if Input.is_action_pressed("UP"):
+#		direction += Vector2(0,-1)
+#	if Input.is_action_pressed("DOWN"):
+#		direction += Vector2(0,1)
+#	if Input.is_action_pressed("LEFT"):
+#		direction += Vector2(-1,0)
+#	if Input.is_action_pressed("RIGHT"):
+#		direction += Vector2(1,0)
+#	move_and_slide(direction * Speed)
 	
-	# keyboard movement
-	if Input.is_action_pressed("UP"):
-		direction += Vector2(0,-1)
-	if Input.is_action_pressed("DOWN"):
-		direction += Vector2(0,1)
-	if Input.is_action_pressed("LEFT"):
-		direction += Vector2(-1,0)
-	if Input.is_action_pressed("RIGHT"):
-		direction += Vector2(1,0)
-	move_and_slide(direction * Speed)
+	# Speed boost
 
-	# mouse movement
-	# position.x += (get_global_mouse_position().x - position.x) / Speed # position.x gets the mouse position.x, subtracts it from the x postion and divides by the Speed
-	# position.y += (get_global_mouse_position().y - position.y) / Speed # position.y gets the mouse position.y, subtracts it from the y postion and divides by the Speed
+
+
+
+#export (int) var speed = 200
+#
+#var velocity = Vector2()
+#
+#func get_input():
+#	look_at(get_global_mouse_position())
+#	velocity = Vector2()
+#	if Input.is_action_pressed("DOWN"):
+#		velocity = Vector2(-speed, 0).rotated(rotation)
+#	if Input.is_action_pressed("UP"):
+#		velocity = Vector2(speed, 0).rotated(rotation)
+#
+#func _physics_process(delta):
+#	get_input()
+#	velocity = move_and_slide(velocity)
+
